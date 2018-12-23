0a84d71722b549d37eaecc3bbe9393753e872b0f
HG King <hgmaxwellking@gmail.com>
2018-12-23 10:07:48 -0600

    working on slices

--- server/server.go
+++ server/server.go
@@ -1,34 +1,53 @@
 package main
 
-import "fmt"
-import "net"
-import "io"
+import (
+    "fmt"
+    "io"
+    "net"
+    "os"
+    "strings"
+)
 
 func main()  {
-  create_server("0.0.0.0:8080")
-}
+    argv := os.Args
+    argc := len(argv)
 
+    if argc != 3 {
+        fmt.Println("Error: Proper Usage -", argv[0], "<host> <port>")
+    } else {
+        create_server("0.0.0.0:8080")
+    }
+}
 
 func create_server(network string)  {
-  /* ln is a Listener */
-  ln, err := net.Listen("tcp", network)
-  if err != nil {
-    fmt.Println("Error creating listener!")
-  } else {
-    fmt.Printf("%T is the type\n", ln)
-    for {
-      conn, err := ln.Accept()
-      if err != nil {
-        fmt.Println("Error starting connection!")
-      } else {
-        fmt.Printf("%T is the type\n", conn)
-        go handle_connection(conn)
-      }
+    /* returns a Listener */
+    ln, err := net.Listen("tcp", network)
+
+    if err != nil {
+        fmt.Println("Error creating listener!")
+    } else {
+        fmt.Printf("%T is the type\n", ln)
+
+        for {
+            conn, err := ln.Accept()
+            if err != nil {
+                fmt.Println("Error starting connection!")
+            } else {
+                fmt.Printf("%T is the type\n", conn)
+            go handle_connection(conn)  // start a go routine to handle conn
+        }
     }
-    ln.Close()
-  }
+
+    ln.Close()  // gotta close the listener
+    }
 }
 
+/*
+*
+*
+*
+*
+*/
 func handle_connection(conn net.Conn)  {
   buf := make([]byte, 4096)
   tmp := make([]byte, 64)     // using small tmo buffer for demonstrating
@@ -45,8 +64,31 @@ func handle_connection(conn net.Conn)  {
     buf = append(buf, tmp[:n]...)
   }
 
-
   fmt.Println("total size:", len(buf))
   fmt.Println(string(buf))
   fmt.Println("DONE")
+}
+
+
+func test_parsing(index *int, str string) []string {
+    slice := str[*index:]
+    crlfPos := strings.Index(slice, "\r\n")
+    fmt.Println(crlfPos, "is what we got")
+    newSlice := str[*index:crlfPos]
+    *index += crlfPos + 2
+    return newSlice
+}
+
+func join_by_reference(sb *strings.Builder, strs ...string) {
+    for _, str := range strs  {
+        (*sb).WriteString(str)
+    }
+}
+
+func join(strs ...string) string {
+    var sb strings.Builder
+    for _, str := range strs  {
+        sb.WriteString(str)
+    }
+    return sb.String()
 }
--- server/test.go
+++ server/test.go
@@ -1,43 +1,108 @@
 package main
-
+import "strings"
 import "fmt"
 
 type MyStruct struct {
-   id int
-	 text string
+    id int
+	text string
 }
 
 func main() {
 	/* this is a comment I hope */
 
-	var i int
-	i = 12
-
 	fmt.Println("Hello, World!")
 
-	fmt.Println(i)
+    // h1 := byte("GET / HTTP/1.1\r\n")
+    // h2 := byte("cache-control: no-cache\r\n")
+    // h3 := byte("Postman-Token: b86db5e0-316f-4b76-8e14-6309004dd961\r\n")
+    // h4 := byte("User-Agent: PostmanRuntime/7.4.0\r\n")
+    // h5 := byte("Accept: */*\r\n")
+    // h6 := byte("Host: www.hgking.xyz:8080\r\n")
+    // h7 := byte("accept-encoding: gzip, deflate\r\n")
+    // h8 := byte("Connection: keep-alive\r\n")
+    //
+    // header_string := join(h1, h2, h3, h4, h5, h6, h7, h8)
+    // n := 0
+    //
+    // for header := test_parsing(&n, header_string); n > 1; {
+    //     fmt.Println(header, "so far n is", n)
+    //     //headers = append(headers, header)
+    // }
+    const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
 
-	foo(&i, "This is j")
+    fmt.Println("Println:")
+    fmt.Println(sample)
 
-	fmt.Println(i)
+    fmt.Println("Byte loop:")
+    for i := 0; i < len(sample); i++ {
+        fmt.Printf("%x ", sample[i])
+    }
+    fmt.Printf("\n")
 
-	var array []int
-	array = make([]int, 3, 5)
+    fmt.Println("Printf with %x:")
+    fmt.Printf("%x\n", sample)
 
-	for j := 0; j < len(array); j++  {
-		fmt.Println(array[j])
-	}
+    fmt.Println("Printf with % x:")
+    fmt.Printf("% x\n", sample)
 
-	var s MyStruct
-	s.id = 10
-	s.text = "The id is ten here"
+    fmt.Println("Printf with %q:")
+    fmt.Printf("%q\n", sample)
 
-	fmt.Println(s.text)
+    fmt.Println("Printf with %+q:")
+    fmt.Printf("%+q\n", sample)
 
 }
 
+// func test_parsing(index *int, str []byte) []byte {
+//     slice := str[*index:]
+//     crlfPos := strings.Index(slice, "\r\n")
+//     fmt.Println(crlfPos, "is what we got")
+//     newSlice := str[*index:crlfPos]
+//     *index += crlfPos + 2
+//     return newSlice
+// }
+
+
+func newline()  {
+    fmt.Println("")
+}
+
+func pop_rear_reference(slice *[]byte) byte {
+  len := len(*slice)
+  val := (*slice)[len - 1]
+  (*slice) = (*slice)[0:len - 1]
+  return val
+}
+
+func pop_rear_value(slice []byte) byte {
+  len := len(slice)
+  val := slice[len - 1]
+  slice = slice[0:len - 1]
+  return val
+}
+
+func AddOneToEachElement(slice []byte) {
+  for i := range slice {
+    slice[i]++
+  }
+}
+
+func join_by_reference(sb *strings.Builder, strs ...string) {
+    for _, str := range strs  {
+        (*sb).WriteString(str)
+    }
+}
+
+func join(strs ...string) string {
+    var sb strings.Builder
+    for _, str := range strs  {
+        sb.WriteString(str)
+    }
+    return sb.String()
+}
+
 //func function_name( [parameter list] ) [return_types]
-func foo(i *int, j string) bool  {
+func foo(i *int, j string) bool {
 	fmt.Println("We are in the subroutine y'all")
 
 	*i++
