5836103c6296ec8c7eb20c1bc02a1854b13f9a2e
HG King <hgmaxwellking@gmail.com>
2019-01-17 12:23:50 -0600

    this stuff hard

--- server/server.go
+++ server/server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net"
-    "os"
-    "strings"
+  "bytes"
+  "fmt"
+  "net"
+  "os"
+  "strings"
 )
 
 func main()  {
@@ -20,26 +20,28 @@ func main()  {
 }
 
 func create_server(network string)  {
-    /* returns a Listener */
-    ln, err := net.Listen("tcp", network)
+  /* returns a Listener */
+  ln, err := net.Listen("tcp", network)
 
-    if err != nil {
-        fmt.Println("Error creating listener!")
-    } else {
-        fmt.Printf("%T is the type\n", ln)
+  if err != nil {
+    fmt.Println("Error creating listener!")
+  } else {
+    fmt.Printf("%T is the type of ln\n", ln)
 
-        for {
-            conn, err := ln.Accept()
-            if err != nil {
-                fmt.Println("Error starting connection!")
-            } else {
-                fmt.Printf("%T is the type\n", conn)
-            go handle_connection(conn)  // start a go routine to handle conn
-        }
+    for {
+      conn, err := ln.Accept()
+
+      if err != nil {
+        fmt.Println("Error starting connection!")
+      } else {
+        fmt.Printf("%T is the type of conn\n", conn)
+        go handle_connection(conn)  // start a go routine to handle conn
+      }
+
     }
 
     ln.Close()  // gotta close the listener
-    }
+  }
 }
 
 /*
@@ -52,9 +54,27 @@ func handle_connection(conn net.Conn)  {
   buf := make([]byte, 4096)
   tmp := make([]byte, 64)     // using small tmo buffer for demonstrating
 
-  var err error
-  n := 1
+  var headers_index []int
+  var headers_length []int
+  result := 0
 
+  for {
+    n, err := conn.Read(tmp)
+    if err != nil {
+      fmt.Println("read error:", err)
+      result = 0
+    } else {
+      fmt.Println("got", n, "bytes.")
+      fmt.Printf("result is %v\n", result)
+      fmt.Println(tmp)
+      buf = append(buf, tmp[:n]...)
+      result = get_headers(&headers_length, &headers_index, &buf, result)
+    }
+    // not proud of the break but golang has forced my hand
+    if result == 0 { break }
+  }
+
+  /*
   for err != io.EOF || n != 0 {
     n, err = conn.Read(tmp)
     if err != nil {
@@ -63,20 +83,99 @@ func handle_connection(conn net.Conn)  {
     fmt.Println("got", n, "bytes.")
     buf = append(buf, tmp[:n]...)
   }
+  */
 
-  fmt.Println("total size:", len(buf))
-  fmt.Println(string(buf))
+  //for each starting index of the headers
+  for i, start := range headers_index {
+    length := headers_length[i]
+    fmt.Println(start, "is the start and is length", length)
+    fmt.Printf("%q\n", buf[start:start+length])
+  }
+
   fmt.Println("DONE")
 }
 
+// headers_index: position in the byte array where the header starts
+// headers_length: length(of course) of the corresponding header
+//                starting at headers_index[i]
+// header_bytes: slice of the request string, maybe not complete
+// index: starting index on the byte string
+//
+// returns 0 on complete headers, index of last header on failure
+//
+func get_headers(headers_length *[]int, headers_index *[]int, header_bytes *[]byte, index int) int {
 
-func test_parsing(index *int, str string) []string {
-    slice := str[*index:]
-    crlfPos := strings.Index(slice, "\r\n")
-    fmt.Println(crlfPos, "is what we got")
-    newSlice := str[*index:crlfPos]
-    *index += crlfPos + 2
-    return newSlice
+  // while there hasn't been a crlf and index is positive(flags an error)
+  for index > -1 && !check_crlf(header_bytes, index) {
+    *headers_index = append(*headers_index, index)
+
+    //read from current index to the next crlf
+    length := header_parsing(&index, header_bytes)
+
+    *headers_length = append(*headers_length, length)
+  }
+
+  // we dont want the last stuff in our headers info
+  if index < 0  {
+    pop_rear_reference_int(headers_length)
+    return pop_rear_reference_int(headers_index)
+  }
+  return 0
+}
+
+// index: pointer used to slice the underlying array of bytes
+//        gets updated by reference to the next header's starting index
+// str: ptr to full string of bytes that the HTTP request resides in
+//
+// returns the length of the partitioned header
+//
+func header_parsing(index *int, str *[]byte) int {
+  // just your typical error checking
+  if *index > -1 && len(*str) != 0 {
+
+    // slice from the specific index till the end
+    slice := (*str)[*index:]
+
+    fmt.Printf("% x with length %d\n", slice, len(slice))
+
+    // make sure the slice still has length
+    if len(slice) != 0 {
+      // grabbing the end of a header
+      // bytes.Index returns -1 on miss
+      i := bytes.Index(slice, []byte("\x0d\x0a"))
+      if i > 0  {
+
+        // index of element before \r\n so +2 offset
+        i += 2
+
+        *index += i
+        return i
+      }
+
+    }
+    // fall through if anything goes wrong
+  }
+  *index = -1
+  return 0
+}
+
+func string_to_bytes(s string) []byte {
+  var bytes = make([]byte, len(s))
+  fmt.Printf("the type of header_bytes is %T\n", bytes)
+  fmt.Printf("the type of header_string is %T\n", s)
+
+  for i := 0; i < len(s); i++ {
+    bytes[i] = byte(s[i])
+  }
+
+  return bytes
+}
+
+func pop_rear_reference_int(slice *[]int) int {
+  len := len(*slice)
+  val := (*slice)[len - 1]
+  (*slice) = (*slice)[0:len - 1]
+  return val
 }
 
 func join_by_reference(sb *strings.Builder, strs ...string) {
@@ -91,4 +190,14 @@ func join(strs ...string) string {
         sb.WriteString(str)
     }
     return sb.String()
+}
+
+func check_crlf(s *[]byte, n int) bool {
+  if len(*s) != 0  {
+    if (*s)[n + 0] == '\x0d' && (*s)[n + 1] == '\x0a'  {
+      //fmt.Println("This appears to be what we're looking for y'all")
+      return true
+    }
+  }
+  return false
 }
--- server/test.go
+++ server/test.go
@@ -1,83 +1,181 @@
 package main
 import "strings"
+import "bytes"
 import "fmt"
 
 type MyStruct struct {
-    id int
+  id int
 	text string
 }
 
-func main() {
-	/* this is a comment I hope */
 
+func main() {
 	fmt.Println("Hello, World!")
 
-    // h1 := byte("GET / HTTP/1.1\r\n")
-    // h2 := byte("cache-control: no-cache\r\n")
-    // h3 := byte("Postman-Token: b86db5e0-316f-4b76-8e14-6309004dd961\r\n")
-    // h4 := byte("User-Agent: PostmanRuntime/7.4.0\r\n")
-    // h5 := byte("Accept: */*\r\n")
-    // h6 := byte("Host: www.hgking.xyz:8080\r\n")
-    // h7 := byte("accept-encoding: gzip, deflate\r\n")
-    // h8 := byte("Connection: keep-alive\r\n")
-    //
-    // header_string := join(h1, h2, h3, h4, h5, h6, h7, h8)
-    // n := 0
-    //
-    // for header := test_parsing(&n, header_string); n > 1; {
-    //     fmt.Println(header, "so far n is", n)
-    //     //headers = append(headers, header)
-    // }
-    const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+  h1 := "GET / HTTP/1.1\r\n"
+  h2 := "cache-control: no-cache\r\n"
+  h3 := "Postman-Token: b86db5e0-316f-4b76-8e14-6309004dd961\r\n"
+  h4 := "User-Agent: PostmanRuntime/7.4.0\r\n"
+  h5 := "Accept: */*\r\n"
+  h6 := "Host: www.hgking.xyz:8080\r\n"
+  h7 := "accept-encoding: gzip, deflate\r\n"
+  h8 := "Connection: keep-alive\r\n"
+  h9 := "content-length: 129"
 
-    fmt.Println("Println:")
-    fmt.Println(sample)
+  header_string := join(h1, h2, h3, h4, h5, h6, h7, h8, h9)
+  header_bytes := string_to_bytes(header_string)
 
-    fmt.Println("Byte loop:")
-    for i := 0; i < len(sample); i++ {
-        fmt.Printf("%x ", sample[i])
+  // these two together will be used to define each header
+  //
+  // header_index: position in the byte array where the header starts
+  // header_length: length(of course) of the corresponding header
+  //                starting at header_index[i]
+  // result:
+  var headers_index []int
+  var headers_length []int
+  var result int
+
+  //for result != 0 {
+    // read()
+    result = get_headers(&headers_length, &headers_index, &header_bytes, result)
+  //}
+
+
+  if result > 0 {
+    fmt.Println("We have not yet finished the request!")
+    fmt.Printf("Starting at %v\n", result)
+
+    //read more!!
+    var str = "\r\nthis is just some random text to test if this works\r\n\r\n"
+    header_string = join(header_string, str)
+    header_bytes = string_to_bytes(header_string)
+
+    result = get_headers(&headers_length, &headers_index, &header_bytes, result)
+    if result > 0 {
+      fmt.Println("We have not yet finished we must keep going!!")
+    } else {
+      fmt.Println("This bitch empty")
     }
-    fmt.Printf("\n")
+  } else {
+    fmt.Println("Our job here is done, time to check for the content-length")
+  }
 
-    fmt.Println("Printf with %x:")
-    fmt.Printf("%x\n", sample)
+  //for each starting index of the headers
+  for i, start := range headers_index {
+    length := headers_length[i]
+    fmt.Println(start, "is the start and is length", length)
+    fmt.Printf("%q\n", header_bytes[start:start+length])
+  }
 
-    fmt.Println("Printf with % x:")
-    fmt.Printf("% x\n", sample)
-
-    fmt.Println("Printf with %q:")
-    fmt.Printf("%q\n", sample)
-
-    fmt.Println("Printf with %+q:")
-    fmt.Printf("%+q\n", sample)
-
+  fmt.Println("Done!")
 }
 
-// func test_parsing(index *int, str []byte) []byte {
-//     slice := str[*index:]
-//     crlfPos := strings.Index(slice, "\r\n")
-//     fmt.Println(crlfPos, "is what we got")
-//     newSlice := str[*index:crlfPos]
-//     *index += crlfPos + 2
-//     return newSlice
-// }
+// headers_index: position in the byte array where the header starts
+// headers_length: length(of course) of the corresponding header
+//                starting at headers_index[i]
+// header_bytes: slice of the request string, maybe not complete
+// index: starting index on the byte string
+//
+// returns 0 on complete headers, index of last header on failure
+//
+func get_headers(headers_length *[]int, headers_index *[]int, header_bytes *[]byte, index int) int {
+
+  // while there hasn't been a crlf and index is positive(flags an error)
+  for index > -1 && !check_crlf(header_bytes, index) {
+    *headers_index = append(*headers_index, index)
+
+    //read from current index to the next crlf
+    length := header_parsing(&index, header_bytes)
+
+    *headers_length = append(*headers_length, length)
+  }
+
+  // we dont want the last stuff in our headers info
+  if index < 0  {
+    pop_rear_reference_int(headers_length)
+    return pop_rear_reference_int(headers_index)
+  }
+  return 0
+}
+
+// index: pointer used to slice the underlying array of bytes
+//        gets updated by reference to the next header's starting index
+// str: ptr to full string of bytes that the HTTP request resides in
+//
+// returns the length of the partitioned header
+//
+func header_parsing(index *int, str *[]byte) int {
+  // just your typical error checking
+  if *index > -1 && len(*str) != 0 {
+
+    // slice from the specific index till the end
+    slice := (*str)[*index:]
+
+    //fmt.Printf("% x with length %d\n", slice, len(slice))
+
+    // make sure the slice still has length
+    if len(slice) != 0 {
+      // grabbing the end of a header
+      // bytes.Index returns -1 on miss
+      i := bytes.Index(slice, []byte("\x0d\x0a"))
+      if i > 0  {
+
+        // index of element before \r\n so +2 offset
+        i += 2
+
+        *index += i
+        return i
+      }
+
+    }
+    // fall through if anything goes wrong
+  }
+  *index = -1
+  return 0
+}
+
+func string_to_bytes(s string) []byte {
+  var bytes = make([]byte, len(s))
+  fmt.Printf("the type of header_bytes is %T\n", bytes)
+  fmt.Printf("the type of header_string is %T\n", s)
+
+  for i := 0; i < len(s); i++ {
+    bytes[i] = byte(s[i])
+  }
+
+  return bytes
+}
 
 
 func newline()  {
-    fmt.Println("")
+  fmt.Println("")
 }
 
-func pop_rear_reference(slice *[]byte) byte {
+func pop_rear_reference_byte(slice *[]byte) byte {
   len := len(*slice)
   val := (*slice)[len - 1]
   (*slice) = (*slice)[0:len - 1]
   return val
 }
 
-func pop_rear_value(slice []byte) byte {
+func pop_rear_value_byte(slice []byte) byte {
   len := len(slice)
   val := slice[len - 1]
   slice = slice[0:len - 1]
+  return val
+}
+
+func pop_rear_value_int(slice []int) int {
+  len := len(slice)
+  val := slice[len - 1]
+  slice = slice[0:len - 1]
+  return val
+}
+
+func pop_rear_reference_int(slice *[]int) int {
+  len := len(*slice)
+  val := (*slice)[len - 1]
+  (*slice) = (*slice)[0:len - 1]
   return val
 }
 
@@ -88,17 +186,27 @@ func AddOneToEachElement(slice []byte) {
 }
 
 func join_by_reference(sb *strings.Builder, strs ...string) {
-    for _, str := range strs  {
-        (*sb).WriteString(str)
-    }
+  for _, str := range strs  {
+    (*sb).WriteString(str)
+  }
 }
 
 func join(strs ...string) string {
-    var sb strings.Builder
-    for _, str := range strs  {
-        sb.WriteString(str)
+  var sb strings.Builder
+  for _, str := range strs  {
+    sb.WriteString(str)
+  }
+  return sb.String()
+}
+
+func check_crlf(s *[]byte, n int) bool {
+  if len(*s) != 0  {
+    if (*s)[n + 0] == '\x0d' && (*s)[n + 1] == '\x0a'  {
+      //fmt.Println("This appears to be what we're looking for y'all")
+      return true
     }
-    return sb.String()
+  }
+  return false
 }
 
 //func function_name( [parameter list] ) [return_types]
